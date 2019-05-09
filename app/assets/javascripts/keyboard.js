d371b90f7517d4a191692cdc7d6a5a05ff08e927
Matias Canepa <macanepa@miuandes.cl>
2019-05-09 17:16:03 -0400

    First Commit

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,57 @@
+class KeyboardController{
+    constructor(target){
+        this.target = target;
+        // console.log(this.target);
+        // console.log(this.target.textContent);
+    }
+
+    UpdateTextbox(key){
+        console.log(this.target.textContent);
+        var text_box_text = this.target.textContent;
+        console.log(key.attr('id'));
+        if (key.attr('id')=="Bksp"){
+            // console.log(this.target[0].lastChild);
+            if(this.target[0].lastChild != null) {
+                this.target[0].lastChild.remove();
+            }
+        }
+        else if (key.attr('id')=="space"){
+            // text_box_text = text_box_text + " ";
+            // this.target.textContent = text_box_text;
+            this.target.append(' ');
+        }
+        else if (key.attr('id')=="enter"){
+            // text_box_text = text_box_text + '<br>';
+            this.target.append('<br>');
+            // this.target.textContent = text_box_text;
+        }
+        else {
+            var key_text = key[0].textContent;
+            key_text = $.trim(key_text);
+            this.target.append(key_text);
+            // text_box_text = text_box_text + key_text;
+            // this.target.textContent = text_box_text;
+        }
+
+    }
+
+}
+
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	keyboard_controller = new KeyboardController($(".wiwi"));
+    $(".key").click(function () {
+        keyboard_controller.UpdateTextbox($(this))
+    });
+
 });
+
+function myFunction() {
+    var x = document.getElementById("kcontainer");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
--- app/controllers/keyboard_demos_controller.rb
+++ app/controllers/keyboard_demos_controller.rb
@@ -1,4 +1,9 @@
 class KeyboardDemosController < ApplicationController
   def demo1
   end
+
+  def demo2
+    render ("keyboard_demos/demo2")
+  end
+
 end
